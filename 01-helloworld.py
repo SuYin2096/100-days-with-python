69d31c2989c061641785a6d0f24d63044bcb7ac3
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-08 22:38:52 +0630

    Complete day-1 lesson.

---
+++ 01-helloworld.py
@@ -0,0 +1,7 @@
+print("SuYin")
+print("March 8th 2024")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print("I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.")
+print("I will be using Replit, an amazing online IDE so I can do this from my phone whereever I happen to be. No excuses for not coding from the middle of a field!")
+print("I am feeling good.")
+print("You can follow my progress at replit.com/@SuYin")
