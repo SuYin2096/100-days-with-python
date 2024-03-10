5db875280b911d3a1cb64cec166a4320a56e6c39
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-10 21:00:30 +0630

    add day3 lesson

---
+++ 03-concatenation.py
@@ -0,0 +1,8 @@
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+cookingMethod = input("Name a method of cooking: ")
+burnedFoodName = input("What word describes burned food?:")
+diyName = input("Name a DIY item: ")
+
+print("MENU")
+print(cookingMethod + " " + food + " with " + burnedFoodName + " " + plant + " on a bed of " + diyName)
