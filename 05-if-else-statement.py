c8c2e315374ca7a97b5dad2a51746c56560b3486
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-12 12:03:37 +0630

    add day 5 lesson

---
+++ 05-if-else-statement.py
@@ -0,0 +1,28 @@
+myName = input("what is your name?: ")
+if myName == "Su Yin":
+    print("Welcome Dude!")
+else:
+    print("Opps! I don't know you")
+
+
+catsOrDog = input("Are you a cat person? Or a dog person?:")
+if catsOrDog == "cat":
+    print("Meow")
+else:
+    print("Woof")
+
+print("Marvel Movie Character Creator")
+print("--")
+
+isLike = input("Do you like 'hanging around'?:")
+if isLike == 'No':
+    print("Then you're not Spider-man.")
+
+isHave = input("Do you have a 'gravelly' voice?: ")
+if isHave == 'No':
+    print("Aww, then you're not Korg.")
+
+isFeel = input("Do you often feel 'Marvelous'?: ")
+if isFeel == 'Yes':
+    print("Aha! you're Captain Marvel! Hi!")
+  
