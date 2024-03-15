986934fe4aa5d2c90d56e316b10682a0f13dd144
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-15 12:54:25 +0630

    add day 6 lesson.

---
+++ 06-elif-statement.py
@@ -0,0 +1,17 @@
+print("MY LOGIN SYSTEM")
+print("++++++++++++++++")
+name = input("Username > ")
+
+if name == "David":
+    password = input("Password > ")
+    print("Why hello there " + name + ", what a lovely acent you have, you could have charmed your way in here even without a password.")
+    print("Have a good day.")
+
+    print("Don't forget to wear a hat in the sun!")
+    
+elif name == "Su":
+    print("this is su, can't login")
+else:
+    print("Dangerous.")
+
+
